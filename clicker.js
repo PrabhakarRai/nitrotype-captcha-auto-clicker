49ed20d3ebad22a3cb1e86aab054f06e91722b64
PrabhakarRai <student343.bhu@gmail.com>
2021-05-14 14:00:07 +0530

    clicks captcha and informs

--- clicker.js
+++ clicker.js
@@ -5,9 +5,8 @@ chrome.storage.sync.get("sleepTime", ({ sleepTime }) => {
   minInterval = sleepTime;
 });
 
-var sid = setInterval(function () {
-  console.log('minInterval', minInterval);
-  console.log('trying');
+// This is for running in reCaptcha iframe
+var sid = setInterval(() => {
   if (
     window.location.href
       .match(/https:\/\/www.google.com\/recaptcha\/api\d\/anchor/) &&
@@ -19,24 +18,26 @@ var sid = setInterval(function () {
       .get(0))
   ) {
     var execute = true;
-    console.log('first if passed');
     if (localStorage.getItem('accesstime')) {
-      console.log('second if passed');
       let timeNow = new Date().getTime();
       let timeLastExecuted = localStorage.getItem('accesstime') | 0;
       let minIntervalMs = minInterval * 60 * 1000;
       if (timeNow - timeLastExecuted < minIntervalMs) {
-        console.log('time is less from prev click');
         execute = false;
       }
     }
 
     if (execute) {
-      console.log('clicked');
       $("#recaptcha-anchor div.recaptcha-checkbox-checkmark").click();
       localStorage.setItem('accesstime', new Date().getTime());
+      setTimeout(async () => {
+        try {
+          await navigator.clipboard.writeText('C_CLICKED');
+        } catch (err) {
+          console.error('Failed to copy: ', err);
+        }
+      }, 0);
     }
     clearInterval(sid);
-    location.href = 'https://www.nitrotype.com/race';
   }
 }, 10000);
